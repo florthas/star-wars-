197b8c3ca0fd93f22f9f983c3a89250ad333862a
florthas <HE201781@students.ephec.be>
2020-08-21 12:54:08 +0200

    presque fini le js

--- programme/js/star-wars.js
+++ programme/js/star-wars.js
@@ -1,9 +1,9 @@
 let tableauxVaisseaux = {
-"venator" : {turboLourd : 56, turboMid : 2, torpilleProt : 4, rayonTract : 6, canonLasers : 52, <img src= "../img/Venator_clonewars.jpg">,  taille : 1155},
-"providence" : {quadTurbo : 14, canonLasers : 34, canonIonLourd : 2, torpilleProt : 102, rayonTract : 8, image : "../img/Providence-class_carrier_1.jpg", taille : 1088},
-"imperial" : {turbolasers : 60, canonIonLourd : 60, rayonTract : 10, image : "../img/Avenger.jpg", taille : 1600},
-"mc80" : {quadTurbo : 30, canonIonLourd : 36, rayonTract : 6, image : "../img/mc80.jpg", taille : 1200},
-"mc85" :{turboLourd : 18, canonIonLourd : 18, canonlasers : 12, torpilleProt : 6, rayontract : 1, image : "../img/mc85.jpg", taille : 3438}
+"venator" : {turboLourd : 56, turboMid : 2, torpilleProt : 4, rayonTract : 6, canonLasers : 52, image: '<img src= "../img/Venator_clonewars.jpg">',  taille : 1155},
+"providence" : {quadTurbo : 14, canonLasers : 34, canonIonLourd : 2, torpilleProt : 102, rayonTract : 8, image : '<img src = "../img/Providence-class_carrier_1.jpg">', taille : 1088},
+"imperial" : {turbolasers : 60, canonIonLourd : 60, rayonTract : 10, image : '<img src = "../img/Avenger.jpg">', taille : 1600},
+"mc80" : {quadTurbo : 30, canonIonLourd : 36, rayonTract : 6, image : '<img src = "../img/mc80.jpg">', taille : 1200},
+"mc85" :{turboLourd : 18, canonIonLourd : 18, canonlasers : 12, torpilleProt : 6, rayontract : 1, image : '<img src = "../img/mc85.jpg">', taille : 3438}
 }
 
 document.addEventListener('DOMContentLoaded', initPage);
@@ -18,43 +18,40 @@ function affichage(event){
 	let choix1 = document.getElementById("vaisseaux1").value
 	let choix2 = document.getElementById("vaisseaux2").value
 	let texteVaisseaux = "<p> vous avez choisi les mêmes vaisseaux</p>"
-	let texteVaisseaux1 = ""
-	let texteVaisseaux2 = ""
-	for (let element in tableauxVaisseaux){
-		texteVaisseaux1 = "<p>Le premier vaisseaux choisi est le :" + tableauxVaisseaux[element] + " : " 
-		texteVaisseaux2 = "<p>Le second vaisseaux choisi est le :" + tableauxVaisseaux[element] + " : " 
-		if (choix1 == tableauxVaisseaux[element]){
+	let texteVaisseaux1 = "<p>Le premier vaisseaux choisi est le " + choix1 + " : " 
+	let texteVaisseaux2 = "<p>Le second vaisseaux choisi est le " + choix2 + " : " 
+		
 			
 			//let texteVaisseaux1 = "<p>Le premier vaisseaux choisi est le :" + tableauxVaisseaux[element] + " : " 
 			//let texteVaisseaux2 = "<p>Le second vaisseaux choisi est le :" + tableauxVaisseaux[element] + " : " 
 			
-			for (element in tableauxVaisseaux[choix1][element]){
-				texteVaisseaux1 += tableauxVaisseaux[choix1][element]
+			for (let elemen in tableauxVaisseaux[choix1]) {
+				texteVaisseaux1 += `${elemen} : ${tableauxVaisseaux[choix1][elemen]}  `
+			}
 				
 				//let texteVaisseaux1 = "<p>Le premier vaisseaux choisi est le :" + tableauxVaisseaux[element] + " : " + "</p>"
 				//document.getElementById("reponse1").innerHTML = texteVaisseaux1
-			}
 			
-			if (choix2 == tableauxVaisseaux[element]){
-				for (element in tableauxVaisseaux[choix2][element]){
-					texteVaisseaux2 += tableauxVaisseaux[choix2][element]
-				}
-				
+			
+			
+				for (let element in tableauxVaisseaux[choix2]) {
+				texteVaisseaux2 += `${element} : ${tableauxVaisseaux[choix2][element]}  `
 				//let texteVaisseaux2 = "<p>Le second vaisseaux choisi est le :" + tableauxVaisseaux[element] + "</p>"
 				//document.getElementById("reponse2").innerHTML = texteVaisseaux2
 				
 			}
-		}
-	}
+		
+	
 	if (choix1 == choix2){
 		document.getElementById("texte").innerHTML = texteVaisseaux
 	}
-	calculTaille (choix1, choix2)
+	let differenceTaille = calculTaille (tableauxVaisseaux [choix1][elemen.length], tableauxVaisseaux[choix2][element.length])
+	document.getElementById("taille").innerHTML = "<p> la difference de taille des deux vaisseaux est de : " + differenceTaille + " m </p>"
 	document.getElementById("reponse1").innerHTML = texteVaisseaux1 + "</p>"
 	document.getElementById("reponse2").innerHTML = texteVaisseaux2 + "</p>"
 	return false
-}
 
+}
 function calculTaille (vaisseaux1, vaisseaux2){
 	let diffTaille
 	if (tableauxVaisseaux[vaisseaux1].taille < tableauxVaisseaux[vaisseaux2].taille ){
