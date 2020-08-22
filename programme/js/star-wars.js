46dd83bdbebd0b2cf58ba40b9e9e7b60009400ba
florthas <HE201781@students.ephec.be>
2020-08-22 13:10:24 +0200

    presque fini

--- programme/js/star-wars.js
+++ programme/js/star-wars.js
@@ -1,7 +1,7 @@
 let tableauxVaisseaux = {
-"venator" : {turboLourd : 56, turboMid : 2, torpilleProt : 4, rayonTract : 6, canonLasers : 52, image: '<img src= "../img/Venator_clonewars.jpg">',  taille : 1155},
-"providence" : {quadTurbo : 14, canonLasers : 34, canonIonLourd : 2, torpilleProt : 102, rayonTract : 8, image : '<img src = "../img/Providence-class_carrier_1.jpg">', taille : 1088},
-"imperial" : {turbolasers : 60, canonIonLourd : 60, rayonTract : 10, image : '<img src = "../img/Avenger.jpg">', taille : 1600},
+"venator" : {turboLourd : 56, turboMid : 2, torpilleProt : 4, rayonTract : 6, canonLasers : 52, image: '<img src= "../img/Venator.jpg">',  taille : 1155},
+"providence" : {quadTurbo : 14, canonLasers : 34, canonIonLourd : 2, torpilleProt : 102, rayonTract : 8, image : '<img src = "../img/Providence.jpg">', taille : 1088},
+"imperial" : {turbolasers : 60, canonIonLourd : 60, rayonTract : 10, image : '<img src = "../img/imperial.jpg">', taille : 1600},
 "mc80" : {quadTurbo : 30, canonIonLourd : 36, rayonTract : 6, image : '<img src = "../img/mc80.jpg">', taille : 1200},
 "mc85" :{turboLourd : 18, canonIonLourd : 18, canonlasers : 12, torpilleProt : 6, rayontract : 1, image : '<img src = "../img/mc85.jpg">', taille : 3438}
 }
